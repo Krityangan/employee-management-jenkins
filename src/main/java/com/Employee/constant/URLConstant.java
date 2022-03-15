b59a3a6dcf87c26a720f1a6ca35abae9e29c2810
Krityangan Rahul <Krityangan.Rahul@mgtechsoft.com>
2022-03-15 23:41:01 +0530

    URL CONSTANT

---
+++ src/main/java/com/Employee/constant/URLConstant.java
@@ -0,0 +1,17 @@
+package com.Employee.constant;
+
+public class URLConstant {
+
+	public final static String FINDALL="/rest/findAll";
+	
+	public final static String SAVE="rest/saveemployee";
+	
+	public final static String FINDBYID="/rest/findById";
+	
+	public final static String FINDCONTACT="/rest/findContactDetailsById";
+	
+	public final static String FINDADDRESS="/rest/findAddressDetailsById";
+	
+	public final static String UPDATE="/rest/updateEmloyeeRecord";
+	
+}
--- src/main/java/com/Employee/controller/EmployeeController.java
+++ src/main/java/com/Employee/controller/EmployeeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Employee.constant.URLConstant;
 import com.Employee.entity.Employee;
 import com.Employee.repo.EmployeeRepository;
 @RestController
@@ -24,7 +25,7 @@ public class EmployeeController {
 	private EmployeeRepository employeeRepository;
 	
 	
-	@RequestMapping(value="rest/saveemployee", method = RequestMethod.POST)
+	@RequestMapping(value=URLConstant.SAVE, method = RequestMethod.POST)
 	public ResponseEntity<?> saveEmployeeDetails(@Valid @RequestBody Employee employee, BindingResult result){
 		
 		if(result.hasErrors())
@@ -36,31 +37,31 @@ public class EmployeeController {
 	
 	
 	
-	@RequestMapping(value="/rest/findAll", method = RequestMethod.GET)
+	@RequestMapping(value=URLConstant.FINDALL, method = RequestMethod.GET)
 	public ResponseEntity<?> getAllEmployee()
 	{
 		return new ResponseEntity<>(employeeRepository.findAll(),HttpStatus.ACCEPTED);
 	}
 	
-	@RequestMapping(value="/rest/findById", method = RequestMethod.GET)
+	@RequestMapping(value=URLConstant.FINDBYID, method = RequestMethod.GET)
 	public ResponseEntity<?> getEmployeeById(@RequestParam(value = "empId") Long empId)
 	{
 		return new ResponseEntity<>(employeeRepository.findById(empId),HttpStatus.ACCEPTED);
 	}
 	
-	@RequestMapping(value="/rest/findContactDetailsById", method = RequestMethod.GET)
+	@RequestMapping(value=URLConstant.FINDCONTACT, method = RequestMethod.GET)
 	public ResponseEntity<?> getEmployeeContactById(@RequestParam(value = "empId") Long empId)
 	{
 		return new ResponseEntity<>(employeeRepository.getContactDetailsById(empId),HttpStatus.ACCEPTED);
 	}
 	
-	@RequestMapping(value="/rest/findAddressDetailsById", method = RequestMethod.GET)
+	@RequestMapping(value=URLConstant.FINDADDRESS, method = RequestMethod.GET)
 	public ResponseEntity<?> getEmployeeAddressById(@RequestParam(value = "empId") Long empId)
 	{
 		return new ResponseEntity<>(employeeRepository.getAddressDetailsById(empId),HttpStatus.ACCEPTED);
 	}
 	
-	@RequestMapping(value="/rest/updateEmloyeeRecord", method = RequestMethod.POST)
+	@RequestMapping(value=URLConstant.UPDATE, method = RequestMethod.POST)
 	public ResponseEntity<?> updateEmployeeRecord(@Valid @RequestBody Employee employee, BindingResult result)
 	{
 		return new ResponseEntity<>(employeeRepository.save(employee),HttpStatus.ACCEPTED);
